8255376740869393ca45f825731965664a5ad24a
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-09-28 16:49:37 +0100

    Esercizio+bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+        name:"Img"
+}
+</script>
+<style>
+div{
+    width: 80%;
+        margin: auto;
+        background-image: url("../assets/jumbo.png");
+  }
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>La mia prima app con VUE CLI!!! </h1>
+</template>
+
+<script>
+export default {
+        name:"Title"
+}
+</script>
+<style>
+    h1{
+        text-align: center;
+        color: red;
+        font-size: 100px;
+    }
+</style>
